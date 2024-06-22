51dac923ceeda6958c59b7981bd6509cd1983c8a
karim <hema_thechamp@yahoo.com>
2024-06-22 19:40:42 -0400

    added home page and scss

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import '../SCSS/App.scss';
+
+const Home = () => {
+    return (
+        <div className="App">
+        <h1 className="title">Ibrahim Karim</h1>
+        </div>
+    )
+}
+
+export default Home;
