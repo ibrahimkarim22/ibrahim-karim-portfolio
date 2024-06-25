b9c213d8b4970c56777baf2e56db86c9e81faca5
karim <hema_thechamp@yahoo.com>
2024-06-24 21:25:18 -0400

    testing meshes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,35 @@
+import { useRef } from 'react';
 import '../SCSS/App.scss';
-import { PerspectiveCamera } from '@react-three/drei';
-import  { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 
+function Cube({ size = [2, 2, 2], speed = 0.01, color = 'steelblue', position = [0, 0, 0] }) {
+    const meshRef = useRef(null);
+
+    useFrame(() => {
+        if (meshRef.current) {
+            meshRef.current.rotation.x += speed;
+            meshRef.current.rotation.y += speed;
+        }
+    });
+
+    return (
+        <mesh ref={meshRef} position={position}>
+            <boxGeometry args={size} />
+            <meshStandardMaterial color={color} />
+        </mesh>
+    );
+}
 
 const Home = () => {
     return (
-    <Canvas>
-      <PerspectiveCamera fov={75} position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
-    )
-}
+        <Canvas style={{ width: '100%', height: '100vh' }}>
+            <ambientLight intensity={1} />
+            <pointLight position={[10, 10, 10]} />
+            <Cube size={[3, 3, 3]} speed={0.003} color="steelblue" position={[0, 0, 0]} />
+            <Cube size={[2, 2, 2]} speed={0.002} color="red" position={[5, 0, 0]} />
+            <Cube size={[1, 1, 1]} speed={0.001} color="green" position={[-5, 0, 0]} />
+        </Canvas>
+    );
+};
 
 export default Home;
