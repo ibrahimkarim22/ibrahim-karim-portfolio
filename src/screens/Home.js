2cb0915c50a728e2232356f5d839ad5ac388edba
karim <hema_thechamp@yahoo.com>
2024-07-10 13:08:21 -0400

    initialized react spring still working on the 3d environment in blender

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,40 @@
 import '../SCSS/App.scss';
 import BlenderEnvironment from './3dEnvironment';
+
+import { animated, useSpring } from '@react-spring/web';
 function Home() {
+
+    const [props, api] = useSpring(() => ({
+        from: { x: 0 }, 
+    }))
+    const handleClick = () => {
+        api.start({
+            from: { 
+                x: 0, 
+            },
+            to: { 
+                x: 800,
+             },
+             delay: 100
+        })
+    }
     return (
-        <div className="blender-environment">
-        <BlenderEnvironment />
-        </div>
+        <>
+            <animated.div
+            onClick={handleClick}
+                style={{
+                    width: 50,
+                    height: 50,
+                    backgroundColor: 'red',
+                    borderRadius: 8,
+                    ...props
+                }}
+            />
+            <div className="blender-environment">
+                <BlenderEnvironment />
+            </div>
+
+        </>
     );
 }
 
