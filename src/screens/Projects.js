675759a4d2894b199e041e766adb87dbf2609dae
karim <hema_thechamp@yahoo.com>
2024-07-24 20:58:03 -0400

    finished adding the nested grids for each project div in the projects page and aligned the titles and descriptions

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -6,18 +6,23 @@ const Projects = () => {
     <div className="projects-div-main">
         <div className="whacka-div-main">
           <div className="whacka-text">Whack a Mole</div>
+          <div className="whacka-description-text">Online Game</div>
         </div>
         <div className="krispy-div-main">
           <div className="krispy-text">KRISPY</div>
+          <div className="krispy-description-text">Streaming Service</div>
         </div>
         <div className="hey-you-div-main">
           <div className="hey-you-text">HeyYou</div>
-        </div>
+          <div className="hey-you-description-text">Location Tracking App</div>
+        </div> 
         <div className="bard-div-main">
           <div className="bard-text">BARD</div>
+          <div className="bard-description-text">Online Course App</div>
         </div>
         <div className="this-portfolio-div-main">
           <div className="this-portfolio-text">This Portfolio</div>
+          <div className="this-portfolio-description-text">This Portfolio</div>
         </div>
       {/* <div className="mmBtn">
         <Link to="/">
