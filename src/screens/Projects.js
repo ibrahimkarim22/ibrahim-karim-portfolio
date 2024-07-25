8e0d879d18f78ec273ede11a5d074aa9aa3fb2c7
karim <hema_thechamp@yahoo.com>
2024-07-25 13:39:04 -0400

    added nav div with headers for projects page and creatd a grid inside the nav. also created a nav footer and created animation for both.

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -4,31 +4,44 @@ import Mm from "../components/Mm";
 const Projects = () => {
   return (
     <div className="projects-div-main">
-        <div className="whacka-div-main">
-          <div className="whacka-text">Whack a Mole</div>
-          <div className="whacka-description-text">Online Game</div>
-        </div>
-        <div className="krispy-div-main">
-          <div className="krispy-text">KRISPY</div>
-          <div className="krispy-description-text">Streaming Service</div>
-        </div>
-        <div className="hey-you-div-main">
-          <div className="hey-you-text">HeyYou</div>
-          <div className="hey-you-description-text">Location Tracking App</div>
-        </div> 
-        <div className="bard-div-main">
-          <div className="bard-text">BARD</div>
-          <div className="bard-description-text">Online Course App</div>
-        </div>
-        <div className="this-portfolio-div-main">
-          <div className="this-portfolio-text">This Portfolio</div>
-          <div className="this-portfolio-description-text">This Portfolio</div>
-        </div>
-      {/* <div className="mmBtn">
-        <Link to="/">
-          <Mm />
+      <div className="nav-div-main">
+        <Link className="nav-home-btn" to={"/"}>
+          <div>Home</div>
         </Link>
-      </div> */}
+        <Link className="nav-resume-btn" to={"/"}>
+          <div>Resume</div>
+        </Link>
+        <Link className="nav-three-dee-resume-btn" to={"/"}>
+          <div>3D Resume</div>
+        </Link>
+        <Link className="nav-contact-container" to="/">
+          <div className="nav-linkedin-btn">Linkedin</div>
+          <div className="nav-github-btn">GitHub</div>
+          <div className="nav-gmail-btn">Gmail</div>
+        </Link>
+      </div>
+      <div className="whacka-div-main">
+        <div className="whacka-text">Whack a Mole</div>
+        <div className="whacka-description-text">Online Game</div>
+      </div>
+      <div className="krispy-div-main">
+        <div className="krispy-text">KRISPY</div>
+        <div className="krispy-description-text">Streaming Service</div>
+      </div>
+      <div className="hey-you-div-main">
+        <div className="hey-you-text">HeyYou</div>
+        <div className="hey-you-description-text">Location Tracking App</div>
+      </div>
+      <div className="bard-div-main">
+        <div className="bard-text">BARD</div>
+        <div className="bard-description-text">Online Course App</div>
+      </div>
+      <div className="this-portfolio-div-main">
+        <div className="this-portfolio-text">This Portfolio</div>
+        <div className="this-portfolio-description-text">This Portfolio</div>
+      </div>
+
+      <div className="footer-div-main"></div>
     </div>
   );
 };
