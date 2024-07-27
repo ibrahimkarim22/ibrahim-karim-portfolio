b851b12c277e8b302d3cd12b31805cc13546ea5d
karim <hema_thechamp@yahoo.com>
2024-07-26 21:32:37 -0400

    almost done with whack a mole project modal added description software logo animations and modal animations also  added software info to projects page I plan on branching out the scss i n.

--- src/components/WhackaModal.js
+++ src/components/WhackaModal.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import js from "../images/js.png";
+import html from "../images/html.png";
+import css from "../images/css.png";
+import sass from "../images/sass.png";
 
 function WhackaModal({ isOpen, closeModal }) {
   return (
@@ -8,19 +12,25 @@ function WhackaModal({ isOpen, closeModal }) {
         <div className="whacka-modal-header-text">Whack a Mole</div>
       </ModalHeader>
       <ModalBody className="whacka-modal-body">
-        <p className="whacka-modal-info">
+        <div className="whacka-modal-info">
           Whack a Mole is a JavaScript game created by my two colleagues
           (Brandon O'Shea and Sam Golshan) and I. Team work though Git and
-          GitHub plus live share was the pinnacle point of this expeirence. It
-          is a 2d game where you have to hit as many moles as you can. Don't
-          forget: you have 1 life. Don't miss the extra points mole. Do Not
+          GitHub plus VS Code Live Share was the most important aspect of this project. It
+          is a 2D game where you have to hit as many moles as you can. Don't
+          forget: you have 1 life. Don't hit the empty holes. Don't miss the extra points mole. Do Not
           click on the Nuke!
-        </p>
+        </div>
+        <img src={js} alt="js logo" className="whacka-modal-js-image" />
+        <img src={html} alt="html logo" className="whacka-modal-html-image" />
+        <img src={css} alt="css logo" className="whacka-modal-css-image" />
+        <img src={sass} alt="sass logo" className="whacka-modal-sass-image" />
       </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={closeModal}>
+      <ModalFooter className="whacka-modal-footer">
+        <Button color="danger" className="modal-close-btn" onClick={closeModal}>
           Close
         </Button>
+        <Button color="warning" className="whacka-play-btn" onClick={src => window.open("https://whackamolewhackamole.web.app", "_blank")}>Play Now!</Button>
+        <Button color="info" className="whacka-modal-github-btn" onClick={src => window.open("https://github.com/ibrahim-karim-22/portfolioprojectgame")}>GitHub</Button>
       </ModalFooter>
     </Modal>
   );
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -38,22 +38,27 @@ const Projects = () => {
       <div className="whacka-div-main" onClick={() => openModal("WhackaModal")}>
         <WhackaModal isOpen={modalOpen.WhackaModal} closeModal={() => closeModal("WhackaModal")} />
         <div className="whacka-text">Whack a Mole</div>
+        <div className="whacka-software-text">JavaScript HTML SCSS SASS</div>
         <div className="whacka-description-text">Online Game</div>
       </div>
       <div className="krispy-div-main">
         <div className="krispy-text">KRISPY</div>
+        <div className="krispy-software-text">JavaScript React Firebase Firestore Redux Bootstrap SCSS SASS</div>
         <div className="krispy-description-text">Streaming Service</div>
       </div>
       <div className="hey-you-div-main">
         <div className="hey-you-text">HeyYou</div>
+        <div className="hey-you-software-text">JavaScript Socket.io Atlas MongoDB Docker GoogleCloud Node.js Express.js</div>
         <div className="hey-you-description-text">Location Tracking App</div>
       </div>
       <div className="bard-div-main">
         <div className="bard-text">BARD</div>
+        <div className="bard-software-text">JavaScript ReactNative Firebase Firestore Redux</div>
         <div className="bard-description-text">Online Course App</div>
       </div>
       <div className="this-portfolio-div-main">
         <div className="this-portfolio-text">This Portfolio</div>
+        <div className="this-portfolio-software-text">JavaScript React Firebase Blender Photoshop ReactFiber Reactstrap SCSS SASS</div>
         <div className="this-portfolio-description-text">This Portfolio</div>
       </div>
       <div className="footer-div-main"></div>
