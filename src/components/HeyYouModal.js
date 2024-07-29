4fe1631437b72962e6c028fc17bff71355f82b31
karim <hema_thechamp@yahoo.com>
2024-07-29 12:44:04 -0400

    started on HeyYou modal added rotation animations for the header body and footer

--- src/components/HeyYouModal.js
+++ src/components/HeyYouModal.js
@@ -1,4 +1,5 @@
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import js from "../images/js.png";
 
 function HeyYouModal({ isOpen, closeModal }) {
   return (
@@ -20,15 +21,19 @@ function HeyYouModal({ isOpen, closeModal }) {
           </div>
         </div>
         <div className="hey-you-modal-info">
+          <p>
+
           Developed a React Native app for real-time location tracking
           and messaging, enhancing seamless communication. Utilized Atlas,
           Docker, Google Cloud, Node.js, Express.js, and Socket.io, achieving
           99.9% data transmission reliability and scalability. Developed
           secure group access keys, boosting data privacy by roughly 30% through
           advanced encryption techniques.
+          </p>
         </div>
+        <img className="js-logo-hey-you" src={js} alt="java script logo" />
       </ModalBody>
-      <ModalFooter className="Hey-you-modal-footer">
+      <ModalFooter className="hey-you-modal-footer">
         <Button
           color="danger"
           className="hey-you-modal-close-btn"
@@ -41,7 +46,7 @@ function HeyYouModal({ isOpen, closeModal }) {
           className="hey-you-request-apk-btn"
           onClick={(src) => window.open("https://drive.google.com/file/d/1qS72H4LG1BF-wKSWfJiPhMNqGe5kkRZ_/view?usp=drive_link", "_blank")}
         >
-          Watch Now!
+          Request APK!
         </Button>
         <Button
           color="info"
