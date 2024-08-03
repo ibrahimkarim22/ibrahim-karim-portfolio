aaafd65575f64617d074fc04012deb82bdb82b8d
karim <hema_thechamp@yahoo.com>
2024-08-03 13:26:40 -0400

    added title, logo, info paragraph for socket io in the realtime updates page also separated scss pages files fro heyYou scss and heyyou keyframes scss using comments with headers for different section on each page this will be the way going forward to not get lost in the scss files

--- src/components/HeyYouModal.js
+++ src/components/HeyYouModal.js
@@ -12,6 +12,10 @@ import express from "../images/express.png";
 import mongo from "../images/mongo.png";
 import node from "../images/node.png";
 import socketio from "../images/socketio.png";
+import socketJoin from "../images/socketJoin.png";
+import socketCreate from "../images/socketCreate.png";
+import socketLocation from "../images/socketLocation.png";
+import socketMessage from "../images/socketMessage.png";
 
 function HeyYouModal({ isOpen, closeModal }) {
   return (
@@ -37,7 +41,7 @@ function HeyYouModal({ isOpen, closeModal }) {
               <div className="bg-light-hey-you-modal"></div>
               <div className="bg-light-hey-you-modal-two"></div>
               <div className="bg-light-hey-you-modal-three"></div>
-              
+
               <div className="node-hey-you-modal-container">
                 <img
                   src={node}
@@ -63,9 +67,9 @@ function HeyYouModal({ isOpen, closeModal }) {
                 Node.js, Express.js, MongoDB Atlas
               </div>
             </div>
-            
-                <div className="hey-you-bg-logo-one">HeyYou</div>
-           
+
+            <div className="hey-you-bg-logo-one">HeyYou</div>
+
             <div className="hey-you-modal-backend-paragraph">
               I developed a React Native app for real-time location tracking and
               messaging. The server was created using Express.js and Node.js,
@@ -74,16 +78,58 @@ function HeyYouModal({ isOpen, closeModal }) {
               the database upon logout to ensure privacy.
             </div>
           </div>
-          <div className='hey-you-modal-realtime-page-container hey-you-page'>
+          <div className="hey-you-modal-realtime-page-container hey-you-page">
+          <div className="real-time-second-bg-container"></div>
             <div className="socket-io-logo-container">
               <img
                 src={socketio}
                 className="socket-io-logo"
                 alt="socket.io logo"
               />
-               </div>
-               <div className="hey-you-modal-realtime-title">Real-Time Updates</div>
-
+            </div>
+           
+           <div className="socket-io-create-img-container">
+              <img 
+              className="socket-io-create-img" 
+              src={socketCreate} 
+              alt="socket.emit createGroup" />
+            </div>
+              <div className="socket-io-join-img-container">
+                <img
+                  src={socketJoin}
+                  className="socket-io-join-img"
+                  alt="socket.emit joinGroup"
+                />
+              </div>
+              <div className="socket-io-location-img-container">
+                <img
+                  src={socketLocation}
+                  className="socket-io-location-img"
+                  alt="socket.emit locationUpdate"
+                />
+              </div>
+              <div className="socket-io-message-img-container">
+                <img
+                  src={socketMessage}
+                  className="socket-io-message-img"
+                  alt="socket.emit messageUpdate"
+                />
+              </div>
+            
+            <div className="hey-you-modal-realtime-title">
+              Real-Time Updates
+            </div>
+            <div className="hey-you-modal-realtime-paragraph">
+              To instantly update users' messages and location coordinates in
+              the MongoDB database and emit that information in real-time, I
+              decided to use Socket.io. Without Socket.io, updates would occur
+              with delays, as the app would have to rely on periodic polling or
+              manual refreshes. Socket.io solves this problem by enabling
+              real-time, bidirectional communication between clients and
+              servers, providing users with the most current information for
+              location tracking, messaging, and group management in the HeyYou
+              app.
+            </div>
           </div>
         </HorizontalScroll>
       </ModalBody>
