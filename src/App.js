ab3796d8f50f15ecc913a44c27fdee126518b100
karim <hema_thechamp@yahoo.com>
2024-08-06 10:58:36 -0400

    deleted the Navbar from react since Ive been creating my own navbars with divs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import "./SCSS/App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Projects from "./screens/Projects";
-import Header from "./components/Header";
 import ThreeDeeResume from "./screens/ThreeDeeResume";
 
 function App() {
   return (
     <Router>
-      {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
