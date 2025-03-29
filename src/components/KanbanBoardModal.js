8fa784351778fa8f07dd7c437d6f66cef9e36061
karim <hema_thechamp@yahoo.com>
2025-03-29 18:30:26 -0400

    added images of the softwares in a grid in the kanban modal

--- src/components/KanbanBoardModal.js
+++ src/components/KanbanBoardModal.js
@@ -1,29 +1,37 @@
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import firebase from "../images/firebase.png";
+import firestore from "../images/firestore.png";
+import sass from "../images/sass.png";
+import js from "../images/js.png";
+import reactLogo from "../images/reactLogo.png";
 
 function KanbanBoardModal({ isOpen, closeModal }) {
-    return (
-        <Modal
-            isOpen={isOpen}
-            toggle={closeModal}
-            fullscreen
-            className='kanban-modal-main-div'
-            >
+  return (
+    <Modal
+      isOpen={isOpen}
+      toggle={closeModal}
+      fullscreen
+      className="kanban-modal-main-div"
+    >
+      {isOpen && (
+        <ModalHeader toggle={closeModal} className="kanban-modal-header">
+          <div className="kanban-modal-header-text">KANBAN BOARD</div>
+          <div className="kanban-modal-header-text-two">To Do List</div>
+        </ModalHeader>
+      )}
+      {isOpen && <ModalBody className="kanban-modal-main">
+        
+          <div className="kanban-software">
+            <img src={js} alt="javaScript" className="kanban-js"/>
+            <img src={sass} alt="sass"  className="kanban-sass" />
+            <img src={reactLogo} alt="React" className="kanban-react" />
+            <img src={firebase} alt="firebase" className="kanban-firebase" />
+            <img src={firestore} alt="firestore" className="kanban-firestore" />
 
-                {isOpen && (
-                    <ModalHeader toggle={closeModal} className='kanban-modal-header'>
-                        <div className='kanban-modal-header-text'>KANBAN BOARD</div>
-                        <div className='kanban-modal-header-text-two'>To Do List</div>
-                    </ModalHeader>
-                )}
-                {isOpen && (
-                    <ModalBody className='kanban-modal-main'>
-                        <h1>Page Under Construction</h1>
-                        <h3>Kanban Board can be accssed from <a href='https://kanbanboardtodolist.web.app/'>here</a></h3>
-                    </ModalBody>
-                )}
-                 {isOpen && (
+          </div>
+        
+        </ModalBody>}
+      {isOpen && (
         <ModalFooter className="kanban-modal-footer">
           <Button
             color="danger"
@@ -33,7 +41,6 @@ function KanbanBoardModal({ isOpen, closeModal }) {
             Close
           </Button>
           <Button
-           
             className="kanban-site-btn"
             onClick={(src) =>
               window.open("https://kanbanboardtodolist.web.app/", "_blank")
@@ -45,17 +52,15 @@ function KanbanBoardModal({ isOpen, closeModal }) {
             color="info"
             className="kanban-modal-github-btn"
             onClick={(src) =>
-              window.open(
-                "https://github.com/ibrahimkarim22/kanbanboard"
-              )
+              window.open("https://github.com/ibrahimkarim22/kanbanboard")
             }
           >
             GitHub
           </Button>
         </ModalFooter>
       )}
-            </Modal>
-    )
+    </Modal>
+  );
 }
 
 export default KanbanBoardModal;
